940a5e7527d257dc6237132251687ff6bd17d354
Shubham-Enata <shubham.damale@autopeepal.com>
2025-09-08 15:35:24 +0530

    done >>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,7 @@ const Hero = () => {
         autoplay
         arrows
         dots={false}
-      // prevArrow={<LeftOutlined className="arrow-btn left" />}
-      // nextArrow={<RightOutlined className="arrow-btn right" />}
+    
       >
         {/* Slide 1 */}
         <div className="hero-slide">
@@ -46,7 +45,7 @@ const Hero = () => {
             </div>
             <div className="hero-middle">
               <h2 className="sprint-text">SPRINT LOADER JR</h2>
-              {/* <h2 className="loader-text">LOADER JR</h2> */}
+            
             </div>
 
             <div className="hero-image">
@@ -79,3 +78,40 @@ const Hero = () => {
 };
 
 export default Hero;
+
+
+// import React from "react";
+// import { Carousel } from "antd";
+// import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+// import Riksha from "../assets/riksha.png";
+// import Test_png from '../assets/test.png'
+// import "./Hero.css";
+
+// const Hero = () => {
+//     return (
+       
+//         <div className="full-bleed">
+//           <Carousel
+//             autoplay
+//             arrows
+//             dots={false}
+//             prevArrow={<LeftOutlined className="arrow-btn left" />}
+//             nextArrow={<RightOutlined className="arrow-btn right" />}
+//           >
+//             <div className="hero-slide">
+//               {/* Text block (sits over the dark band) */}
+//               <div className="hero-text">
+//                 <h1>MEET THE FUTURE OF URBAN MOBILITY</h1>
+//                 <p>ELECTRIC. EFFICIENT. AFFORDABLE.</p>
+//               </div>
+
+//               {/* Rickshaw image anchored bottom-right */}
+//               <img className="hero-riksha" src={Riksha} alt="Rickshaw" />
+//             </div>
+//           </Carousel>
+//         </div>
+//         // <img src={Test_png} height={700} width={1200} ></img>
+//     );
+// };
+
+// export default Hero;
